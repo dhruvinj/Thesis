bdf2fd95eb2d36643fc280b6df51a9243e2d5c28
dhruvin <dhruvin1991@gmail.com>
2016-01-03 23:23:51 -0500

    results updated

---
+++ latex/multiple_data/20_kde/outputData_1e7/convert_mh_data_to_hdf5.py
@@ -0,0 +1,11 @@
+import h5py
+import numpy
+
+rawdata = numpy.loadtxt("testdata.dat")
+
+h5file = h5py.File("chain_data.h5", "w")
+
+h5file.create_dataset("/zoidberg1/data/users/dhruvinj/multiple_data/20_kde/outputData_1e7/E2", data=rawdata[:,0])
+
+h5file.create_dataset("/zoidberg1/data/users/dhruvinj/multiple_data/20_kde/outputData_1e7/E3", data=rawdata[:,1])
+
---
+++ latex/one_parameter/flamespeed_calculation/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo -e "Solving for flamespeed"
+mpiexec -np 4 ./1Run 
+
+rm *.xml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
